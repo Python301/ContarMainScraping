95bd3db6b8c0463bd0f17a1d55c3feded0673341
B.Yedukondalu <108857090+Python301@users.noreply.github.com>
2026-09-23 11:10:11 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -5,3 +5,30 @@ DB_CONFIG = {
     'username' : 'hyd_scrape',
     'password' : 'hyd$cr@93#@@@'
 }
+
+
+
+try:
+    price_text = page.locator("span.cp-mw__final").first.text_content(timeout=2000)
+    match = re.search(r"\d+\.\d+", price_text)
+
+    if match:
+        mprice = match.group()
+    else:
+        mprice = 0.0
+
+except Exception:
+    try:
+        price_text = page.locator("span.MembersPrice").first.text_content(timeout=2000)
+        match = re.search(r"\d+\.\d+", price_text)
+
+        if match:
+            mprice = match.group()
+        else:
+            mprice = 0.0
+
+    except Exception as e:
+        print("Error occurred:", e)
+        mprice = 0.0
+
+print("Member Price:", mprice)
